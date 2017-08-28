ef54d153b6abab0f8b12c1892eed07f41e830c5a
Alexander Murphy <amurphy345@gmail.com>
2017-08-27 20:24:30 -0600

    Initial Commit

---
+++ ui_gradients_viewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ui_gradients_viewer
+//
+//  Created by Alexander Murphy on 8/27/17.
+//  Copyright © 2017 Alexander Murphy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ui_gradients_viewerTests/ui_gradients_viewerTests.swift
@@ -0,0 +1,36 @@
+//
+//  ui_gradients_viewerTests.swift
+//  ui_gradients_viewerTests
+//
+//  Created by Alexander Murphy on 8/27/17.
+//  Copyright © 2017 Alexander Murphy. All rights reserved.
+//
+
+import XCTest
+@testable import ui_gradients_viewer
+
+class ui_gradients_viewerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
