7ed5f33d90197e6f98a9f22608c7d4d61d5bbaae
Alexander Murphy <amurphy345@gmail.com>
2018-06-16 18:24:28 -0600

    Revert "adding anchorage"
    This reverts commit 7e11d017a4ee6c4cc59c9dd9c26916349d8bddd0.

--- Pods/Target Support Files/Pods-ui_gradients_viewer/Pods-ui_gradients_viewer-frameworks.sh
+++ Pods/Target Support Files/Pods-ui_gradients_viewer/Pods-ui_gradients_viewer-frameworks.sh
@@ -143,14 +143,12 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/Anchorage/Anchorage.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GradientView/GradientView.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Pageboy/Pageboy.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Pulley/Pulley.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftHEXColors/SwiftHEXColors.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/Anchorage/Anchorage.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GradientView/GradientView.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Pageboy/Pageboy.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Pulley/Pulley.framework"
--- ui_gradients_viewer/Controllers/GradientDrawerViewController.swift
+++
@@ -1,5 +0,0 @@
-import UIKit
-
-final class GradientDrawerViewController: UIViewController {
-    
-}
