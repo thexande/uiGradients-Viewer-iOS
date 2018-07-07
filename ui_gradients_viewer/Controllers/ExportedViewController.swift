83460ef6cec1c315f5f0df87e9e1eceafb968244
Alexander Murphy <amurphy345@gmail.com>
2018-07-07 00:35:33 -0600

    adding confetti view

---
+++ ui_gradients_viewer/Controllers/ExportedViewController.swift
@@ -0,0 +1,5 @@
+import UIKit
+
+final class ExportedViewController: UIViewController {
+     let confettiView = SAConfettiView(properties: ConfettiCardProperties(colorsNodes: true, colors: ConfettiCardProperties.defaultColors, type: .confetti))
+}
